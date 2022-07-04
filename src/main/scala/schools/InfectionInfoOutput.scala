d4aee78bcd6c8f4c6f76222d990bf56d435b791d
Riz <87264061+rizfn@users.noreply.github.com>
2022-07-04 17:11:11 +0530

    Optimizations + misc. jar stuff
    1. Added gaurav's optimization in the output files
    2. Changed build.sbt and application.conf to get the sources to compile
    3. Changed the input parameters and defaults to better suit my needs

--- src/main/scala/schools/InfectionInfoOutput.scala
+++ src/main/scala/schools/InfectionInfoOutput.scala
@@ -4,6 +4,9 @@ import com.bharatsim.engine.Context
 import com.bharatsim.engine.basicConversions.encoders.DefaultEncoders._
 import com.bharatsim.engine.graph.patternMatcher.MatchCondition._
 import com.bharatsim.engine.listeners.CSVSpecs
+import schools.InfectionStatus._
+
+import scala.collection.mutable
 
 class InfectionInfoOutput(context: Context) extends CSVSpecs {
 
@@ -24,19 +27,31 @@ class InfectionInfoOutput(context: Context) extends CSVSpecs {
   override def getRows(): List[List[Any]] = {
     val graphProvider = context.graphProvider
     val label = "Person"
+    val infectedByMap = mutable.HashMap.empty[String, Int]
+    val infectedAtMap = mutable.HashMap.empty[String, Int]
+    val nodes = graphProvider.fetchNodes(label)
+    nodes.foreach(node => {
+      val infectedBy = node.getParams.apply("wasInfectedBy").toString
+      val existingInfectedByCount = infectedByMap.getOrElse(infectedBy, 0)
+      infectedByMap.put(infectedBy, existingInfectedByCount + 1)
+      val infectedAt = node.getParams.apply("wasInfectedAt").toString
+      val existingInfectedAtCount = infectedAtMap.getOrElse(infectedAt, 0)
+      infectedAtMap.put(infectedAt, existingInfectedAtCount + 1)
+    })
+
     val row = List(
-      context.getCurrentStep * Main.dt,
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "Asymptomatic"),
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "Presymptomatic"),
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "InfectedMild"),
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "InfectedSevere"),
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "Hospitalised"),
-      graphProvider.fetchCount(label, "wasInfectedBy" equ "FOI"),
-      graphProvider.fetchCount(label, "wasInfectedAt" equ "Home"),
-      graphProvider.fetchCount(label, "wasInfectedAt" equ "Office"),
-      graphProvider.fetchCount(label, "wasInfectedAt" equ "ClassRoom"),
+      context.getCurrentStep,
+      infectedByMap.getOrElse(Asymptomatic.toString, 0),
+      infectedByMap.getOrElse(Presymptomatic.toString, 0),
+      infectedByMap.getOrElse(InfectedMild.toString, 0),
+      infectedByMap.getOrElse(InfectedSevere.toString, 0),
+      infectedByMap.getOrElse(Hospitalised.toString, 0),
+      infectedByMap.getOrElse("FOI", 0),
+      infectedAtMap.getOrElse("Home", 0),
+      infectedAtMap.getOrElse("Office", 0),
+      infectedAtMap.getOrElse("ClassRoom", 0)
     )
-    List(row)
+    return List(row)
 
   }
 
